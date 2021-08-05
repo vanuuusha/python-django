c35f4686d035c5b6baa4e85b08f65935a2c94e81
Alexander Olhovik <aao.t@yandex.ru>
2021-08-05 17:07:21 +0300

    урок 1, зачёт

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -5,11 +5,13 @@ from django.views import View
 from random import choice
 
 TASKS = ['проснуться', 'позавтракать', 'пойти что-то делать', 'каточка в кс', 'пойти спать', 'скачать Pycharm', 'Установить django', 'Запустить сервер', 'Порадоваться результату']
+# TODO Не забывайте про стиль кода - ограничение длины строки кода в 120 символов (а ещё лучше 80)
+
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        tasks = TASKS[:] # для того, чтобы не повторялись задачи
+        tasks = TASKS[:]  # для того, чтобы не повторялись задачи
         server_answer = '<ul>'
         for _ in range(5):
             now_task = choice(tasks)
