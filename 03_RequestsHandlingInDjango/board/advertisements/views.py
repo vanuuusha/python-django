77f9fa83711f9bbdde9ebfe24929b1170b3a16a4
Vanusha <ivan.korotchenya@gmail.com>
2021-08-09 13:25:34 +0300

    module 3 corrected

--- 03_RequestsHandlingInDjango/board/advertisements/views.py
+++ 03_RequestsHandlingInDjango/board/advertisements/views.py
@@ -13,24 +13,25 @@ class MainPage(View):
 
 ip_checker = {}
 
-class ListAdvertisement(View):  # TODO Не забывайте про стиль кода по РЕР 8
+
+class ListAdvertisement(View):
+
     def get(self, request):
         ip = request.META.get("REMOTE_ADDR")
-        ip_checker[ip] = {'get': ip_checker.get(ip, {'get': 0})['get'], 'post': ip_checker.get(ip, {'post': 0})['post']}
-        if request.method == 'GET':
-            ip_checker[ip]['get'] += 1
-        else:
-            ip_checker[ip]['post'] += 1  # TODO Не возможно, вы же пишите этот код в методе get() представления который
-                                         #  обрабатывает только GЕТ-запросы. Перенесите это в метод post
+        ip_checker[ip] = {'get': ip_checker.get(ip, {'get': 0})['get'] + 1, 'post': ip_checker.get(ip, {'post': 0})['post']}
         now_count = ip_checker[ip]
-        gdz = ['Python Basic', 'Python-фреймворк Django', 'frontend Разработчик', "Веб-верстка «Базовый уровень»", 'java - Разработчик']
+        gdz = ['Python Basic', 'Python-фреймворк Django', 'frontend Разработчик', "Веб-верстка «Базовый уровень»",
+               'java - Разработчик']
         another = ['contacts', 'about-us', 'categories', 'regions']
         films = ['Довод', 'Душа', "Еще по одной", "Огонь"]
         songs = ['Like a Rolling Stone', '(I Can’t Get No) Satisfaction', 'Imagine', 'Good Vibrations']
         info_list = [gdz, another, films, songs]
-        return render(request, 'advertisements/my_list_advertisement.html', {'info_list': info_list, 'now_count': now_count})
+        return render(request, 'advertisements/my_list_advertisement.html',
+                      {'info_list': info_list, 'now_count': now_count})
 
     def post(self, request):
+        ip = request.META.get("REMOTE_ADDR")
+        ip_checker[ip] = {'get': ip_checker.get(ip, {'get': 0})['get'], 'post': ip_checker.get(ip, {'post': 0})['post'] + 1}
         return HttpResponse('запрос на создание новой записи успешно выполнен')
 
 
@@ -44,6 +45,7 @@ class Contacts(TemplateView):
         context['email'] = 'megacompany@gmail.com'
         return context
 
+
 class About(TemplateView):
     template_name = 'advertisements/about.html'
 
@@ -52,5 +54,3 @@ class About(TemplateView):
         context['company_name'] = 'Булочная'
         context['company_about'] = 'Самый вкусный булочки'
         return context
-
-
